cc5fe5b4f03221459345d73b8537e1bf332f36df
0xSc0rpi0n <55562628+Calicanx@users.noreply.github.com>
2024-06-12 11:06:32 +0000

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,56 @@ use yii\widgets\ActiveForm;
 /** @var app\models\Leads $model */
 /** @var ActiveForm $form */
 ?>
-<div class="index">
-  <h1>Index</h1>
-
-</div><!-- index -->
+<div class="container">
+  <div class="row header">
+    <div class="col-sm-12">
+      <span>Company Name</span>
+    </div>
+  </div>
+  <div class="row nav-row">
+    <div class="col-sm-12">
+      <nav class="nav">
+        <ul>
+          <li class="dropdown ">
+            <a href="#" id="dropdownMenuLink" data-toggle="dropdown" class="dropdown-toggle active">CRM</a>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <ll>
+                <a href="#">Customers</a>
+          </li>
+          <ll>
+            <a href="#">Firms</a>
+            </li>
+            <ll>
+              <a href="#">Branches</a>
+              </li>
+              <ll>
+                <a href="#">Invoices</a>
+                </li>
+                <ll>
+                  <a href="#">Live Classes</a>
+                  </li>
+                  <ll>
+                    <a href="#">Pending Orders</a>
+                    </li>
+                    <ll>
+                      <a href="#">My Accounts</a>
+                      </li>
+                      </ul>
+                      </li>
+                      <li><a href="#">Content Admin</a></li>
+                      <li><a href="#">Content Support</a></li>
+                      <li><a href="#">Account Management</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-12">
+      <h3 class="text-center">
+                  Landing Page</h3>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-6 btns"> <?= Html::a('View All Records', ['site/viewall'], ['class' => 'btn btn-secondary btn-lg']) ?> <?= Html::a('Add customer/lead record', ['site/create'], ['class' => 'btn btn-secondary btn-lg']) ?> </div>
+  </div>
+</div>
